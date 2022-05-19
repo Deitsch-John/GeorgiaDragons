6c690e2e4a8f912c0ee147b3bf0449c10a70beca
Deitsch-John <jfd77@cornell.edu>
2022-05-18 21:54:57 -0400

    Version 1 complete

--- dragons/app.R
+++ dragons/app.R
@@ -26,7 +26,13 @@ dobbs_data <- read_csv("mdobbs.csv")%>%
   rename(name = `...2`)%>%
   select(-`...3`, -`...163`, -`...163`)%>%
   mutate_all(~replace(., is.na(.), 0))%>%
-  pivot_longer(Appling:Worth, names_to = "County", values_to = "Observed")
+  pivot_longer(Appling:Worth, names_to = "County", values_to = "Observed")%>%
+  filter(Observed %in% c("X", "0"))%>%
+  mutate(Observed = case_when(
+    Observed=="X^"~"X",
+    Observed=="X"~"X",
+    Observed=="0"~"0"
+  ))
 
 GeorgiaCounties <- UScounties%>%
   filter(STATE%in%c("GA"))%>%
@@ -81,13 +87,18 @@ SpeciesMap <- function(speciestomap) {
     mutate(Present = ifelse(is.na(Observations), "No", "Yes"))%>%
     left_join(TaxaData.dobbs)%>%
     select(-species, -name)%>%
-    mutate(Present_dobbs = ifelse(Present=="No" & Observed=="X", "Yes", Present))
+    mutate(Present_dobbs = case_when(
+      Present=="Yes" & Observed=="X"~"Observed",
+      Present=="Yes" & Observed=="0"~"Observed",
+      Present=="No" & Observed=="X"~"Observed",
+      Present=="No" & Observed=="0"~"Not Observed"
+    ))
 
   attach(TaxaToMap)
   
   TaxaMap <- ggplot()+
     geom_sf(data = TaxaToMap, 
-            fill = ifelse(Present_dobbs=="Yes", "seagreen3", "ivory1"), 
+            fill = ifelse(Present_dobbs=="Observed", "seagreen3", "ivory1"), 
             color = "grey41", 
             size = 0.10)+
     theme(
@@ -102,50 +113,6 @@ SpeciesMap <- function(speciestomap) {
     labs(title = speciestomap)
   
   return(TaxaMap)
-}
-county_table <- function(speciestomap){
-  TaxaData <- data.sf %>%
-    filter(Species_cleaned==speciestomap)
-  
-  TaxaByCounty <- st_join(TaxaData, GeorgiaCounties)%>%
-    st_drop_geometry()%>%
-    group_by(County)%>%
-    summarize(Observations = n())%>%
-    filter(!is.na(County))%>%
-    arrange(-Observations)%>%
-    gt::gt()%>%
-    tab_header(
-      title = str_c("County level data for ", speciestomap, "."),
-      subtitle = str_c("There are ", nrow(TaxaData), " observations in this dataset.")
-    )%>%
-  tab_source_note(
-    source_note = "This table is based on iNaturalist and Odonata Central observation data as well as some museum specimen records downloaded from the Global Biodiversity Information Facility. This table does not include data from the archive compiled by Marion Dobbs."
-  )
-  
-  return(TaxaByCounty)
-}
-species_list <- function(county){
-  
-  CountyList <- st_join(data.sf, GeorgiaCounties)%>%
-    st_drop_geometry()%>%
-    filter(County==county)%>%
-    dplyr::select(Species_cleaned)%>%
-    group_by(Species_cleaned)%>%
-    summarize(Observations = n())%>%
-    arrange(Species_cleaned)%>%
-    rename(Species = Species_cleaned)
-  
-  table <- CountyList%>%
-    gt::gt()%>%
-    tab_header(
-      title = str_c(county, " county species list."),
-      subtitle = str_c(nrow(CountyList), " species observed.")
-    )%>%
-    tab_source_note(
-      source_note = "This table is based on iNaturalist and Odonata Central observation data as well as some museum specimen records downloaded from the Global Biodiversity Information Facility. This table does not include data from the archive compiled by Marion Dobbs."
-    )
-  
-  return(table)
 }
 CountyMap <- function(county){
   obs_county <- st_join(data.sf, GeorgiaCounties)%>%
@@ -181,6 +148,137 @@ CountyMap <- function(county){
                            location = "tr")+
     labs(title = county)
   
+}
+dobbs_species_list <- function(county){
+  list.df <- dobbs_data %>%
+    filter(Observed=="X")%>%
+    filter(County==county)%>%
+    select(species, Observed)
+  
+  return(list.df)
+}
+dobbs_county_table <- function(speciestomap){
+  list.df <- dobbs_data %>%
+    filter(Observed=="X")%>%
+    filter(species==speciestomap)%>%
+    select(Observed, County)
+  
+  return(list.df)
+}
+county_table <- function(speciestomap){
+  OnlineData <- data.sf %>%
+    filter(Species_cleaned==speciestomap)
+  
+  OnlineByCounty <- st_join(OnlineData, GeorgiaCounties)%>%
+    st_drop_geometry()%>%
+    group_by(County)%>%
+    summarize(Observations = n())%>%
+    filter(!is.na(County))
+  
+  DobbsList <- dobbs_county_table(speciestomap)
+  
+  FullList <- full_join(OnlineByCounty, DobbsList)%>%
+    mutate(Observations2 = ifelse(is.na(Observations)==TRUE, "Not reported online", Observations))%>%
+    select(County, Observations2)%>%
+    rename(Observations=Observations2)
+  
+  Table <- FullList %>%
+    arrange(County)%>%
+    gt::gt()%>%
+    tab_header(
+      title = str_c("County level data for ", speciestomap, "."),
+      subtitle = str_c(speciestomap, " has been observed in ", nrow(FullList), " counties in Georgia.")
+    )%>%
+    tab_source_note(
+      source_note = "Taxa with no online observations or museum specimens are sourced from an incredible data archive compiled by Marion Dobbs."
+    )
+  
+  return(Table)
+}
+species_list <- function(county){
+  
+  OnlineList <- st_join(data.sf, GeorgiaCounties)%>%
+    st_drop_geometry()%>%
+    filter(County==county)%>%
+    dplyr::select(Species_cleaned)%>%
+    group_by(Species_cleaned)%>%
+    summarize(Observations = n())%>%
+    arrange(Species_cleaned)%>%
+    rename(Species = Species_cleaned)%>%
+    filter(!is.na(Species))
+  
+  DobbsList <- dobbs_species_list(county)%>%
+    rename(Species=species)
+  
+  FullList <- full_join(OnlineList, DobbsList)%>%
+    mutate(Observations2 = ifelse(is.na(Observations)==TRUE, "Not reported online", Observations))%>%
+    select(Species, Observations2)%>%
+    rename(Observations=Observations2)
+  
+  table <- FullList %>%
+    arrange(Species)%>%
+    gt::gt()%>%
+    tab_header(
+      title = str_c(county, " county species list."),
+      subtitle = str_c(nrow(FullList), " species recorded.")
+    )%>%
+    tab_source_note(
+      source_note = "Taxa with no online observations or museum specimens are sourced from an incredible data archive compiled by Marion Dobbs."
+    )
+  
+  return(table)
+}
+DiversityMap <- function(dfx){
+  online <- st_join(dfx, GeorgiaCounties)%>%
+    st_drop_geometry()%>%
+    select(Species_cleaned, County)%>%
+    rename(species=Species_cleaned)
+  
+  dobbs <- dobbs_data %>%
+    filter(Observed=="X")%>%
+    select(species, County)
+  
+  full <- bind_rows(online, dobbs)
+  
+  full_stats <- full %>%
+    group_by(County)%>%
+    summarize(Species = length(unique(species)))%>%
+    filter(!is.na(County))
+  
+  full_stats_mapping <- GeorgiaCounties %>%
+    left_join(full_stats)%>%
+    mutate(Diversity = case_when(
+      Species >= 90 ~ "Very High (90+)",
+      Species >= 70 & Species < 90 ~ "High (70-89)",
+      Species >= 40 & Species < 70 ~ "Medium (40-69)",
+      Species > 15 & Species < 40 ~ "Low (16-39)",
+      Species <= 15 ~ "Low Data (<15)",
+      is.na(Species)=="TRUE"~"No Data"
+    ))
+  
+  full_stats_mapping$Diversity <- factor(full_stats_mapping$Diversity,
+                                         levels = c("Very High (90+)", "High (70-89)",
+                                                    "Medium (40-69)", "Low (16-39)", 
+                                                    "Low Data (<15)", "No Data"))
+  
+  Map <- ggplot()+
+    geom_sf(data = full_stats_mapping, aes(fill = Diversity), 
+            color = "grey41", 
+            size = 0.10)+
+    scale_fill_brewer(type = "qual", palette = "Spectral", 
+                      direction = 1)+
+    theme(
+      panel.grid.major = element_line(linetype = "dashed", 
+                                      color = "grey88"),
+      panel.background = element_rect(fill = "aliceblue"),
+      panel.border = element_rect(fill = NA, size = 2))+
+    annotation_scale(location="bl")+
+    annotation_north_arrow(which_north = "true",
+                           style = north_arrow_nautical,
+                           location = "tr")+
+    labs(title = "Species Diversity of Odonates in Georgia")
+  
+  return(Map)
 }
 
 SpeciesList <- data.sf%>%
@@ -221,7 +319,11 @@ ui <- fluidPage(
                     plotOutput("Map_county")
             )
     )
-  )
+  ),
+    tabPanel("Statewide Species Diversity",
+             fluidRow(column(12,
+                             plotOutput("Map_diversity")
+                             )))
 )
 )
 
@@ -231,6 +333,7 @@ server <- function(input, output, session) {
   output$Summary_Stats <- render_gt(county_table(input$species))
   output$County_List <- render_gt(species_list(input$county))
   output$Map_county <- renderPlot(CountyMap(input$county))
+  output$Map_diversity <- renderPlot(DiversityMap(data.sf))
 }
 
 
